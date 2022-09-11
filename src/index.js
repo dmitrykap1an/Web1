758448a17b9746737aee9a3086e066244fe81b48
dmitrykap1an <dk475641@gmail.com>
2022-09-12 02:43:04 +0300

    added new functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-import './index.css'
 
 let xButtons = document.querySelectorAll(".x_button");
 let rButtons = document.querySelectorAll(".r-button");
 let yButton = document.querySelector(".y_text");
-let submit = document.querySelector(".submit");
 let form = document.getElementById("form")
-const request = new XMLHttpRequest()
+let table = document.querySelector(".my_table")
+const tbody = table.getElementsByTagName("tbody")[0];
 
-form.onsubmit = function (){
+form.onsubmit= function (){
 
     let x = checkX()
     let y = validateY()
     let r = checkR()
     let array = [x, y, r]
     if(x.status && y.status && r.status){
-        console.log(array)
+        sendRequest([x.value, y.value, r.value])
     }
     else{
         let errorString = ""
@@ -26,7 +25,6 @@ form.onsubmit = function (){
 
         alert(errorString)
     }
-    return false
 }
 
 function validateY(){
@@ -106,9 +104,52 @@ function checkR(){
         errorMessage: ""
     }
 }
+function updateTable(response) {
+    const row = document.createElement("tr");
+    const cell_x = document.createElement("td");
+    const cell_y = document.createElement("td");
+    const cell_R = document.createElement("td");
+    const cell_hit = document.createElement("td");
+    const cell_request_time = document.createElement("td");
+    const cell_timing = document.createElement("td");
+    cell_x.innerHTML = response.x;
+    cell_y.innerHTML = response.y;
+    cell_R.innerHTML = response.R;
+    cell_hit.innerHTML = response.result;
+    cell_request_time.innerHTML = response.server_time;
+    cell_timing.innerHTML = response.script_exec_time + "ms";
+    row.appendChild(cell_x);
+    row.appendChild(cell_y);
+    row.appendChild(cell_R);
+    row.appendChild(cell_hit);
+    row.appendChild(cell_request_time);
+    row.appendChild(cell_timing);
+    tbody.insertBefore(row, tbody.firstChild);
+}
 
-function send(array){
-    request.open("GET", "https://kaplaan.ru/script.php")
-    request.send(array)
+function getResponse(request){
+    console.log(request.responseText);
+    const response = JSON.parse(request.responseText)
+    if (response.correct === "true") {
+        updateTable(response);
+    } else {
+        alert("Ошибка обработки запроса сервером");
+    }
+
+}
+function sendRequest(array){
+
+    const path = "script.php?x=" + array[0].value + "&y=" + array[1].value + "&R=" + array[2].value
+    const request = new XMLHttpRequest()
+    request.open("GET", path, true)
+
+    request.responseType = "text"
+    request.setRequestHeader("Content-Type", "application/x-www-form-url")
+    request.onreadystatechange = () =>{
+        if(request.readyState === 4 && request.status === 200){
+            getResponse(request)
+        }
+    }
+    request.send()
 
 }
