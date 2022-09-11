7b1a41ce75463cf69e035abf3a7c00c511e76eeb
dmitrykap1an <dk475641@gmail.com>
2022-09-11 22:23:07 +0300

    added new functionality

--- src/index.js
+++ src/index.js
@@ -5,23 +5,110 @@ let rButtons = document.querySelectorAll(".r-button");
 let yButton = document.querySelector(".y_text");
 let submit = document.querySelector(".submit");
 let form = document.getElementById("form")
+const request = new XMLHttpRequest()
 
 form.onsubmit = function (){
-    let yVal = yButton.value
-    xButtons.forEach( function (input){
-        if(input.matches(":checked")){
-            console.log(input.value)
+
+    let x = checkX()
+    let y = validateY()
+    let r = checkR()
+    let array = [x, y, r]
+    if(x.status && y.status && r.status){
+        console.log(array)
+    }
+    else{
+        let errorString = ""
+        array.forEach(function (input){
+            if(!input.status){
+                errorString += input.errorMessage + "\n"
+            }
+        })
+
+        alert(errorString)
+    }
+    return false
+}
+
+function validateY(){
+    let yVal = yButton.value.replace(",", ".")
+    if(!isNaN(yVal)){
+        return checkY(Math.round(Number.parseFloat(yVal) * 1000) / 1000)
+    }
+    else{
+        return {
+            status: false,
+            value: 404,
+            errorMessage: "Y должен быть представлен числом"
         }
-    })
-    if(yVal >= -5 && yVal <= 3){
-        console.log(yVal)
+    }
+}
+
+function checkY(yVal){
+    if(yVal >= -5 && yVal <= 3 && yVal !== ""){
+        return {
+            status: true,
+            value: yVal,
+            errorMessage: ""
+        }
+    }
+    else{
+        return {
+            status: false,
+            value: 404,
+            errorMessage: "Число Y должно быть в промежутке от -5 до 3"
+        }
     }
 
-    rButtons.forEach(function (input){
+}
+
+function checkX(){
+    let number = 404;
+    xButtons.forEach(function (input){
         if(input.matches(":checked")){
-            console.log(input.value)
+            number = Number.parseFloat(input.value);
         }
     })
 
-    return false
+    if(number === 404){
+        return {
+            status: false,
+            value: number,
+            errorMessage: "Выберете X!"
+        }
+    }
+    else return {
+        status: true,
+        value: number,
+        errorMessage: ""
+    }
+
+
+}
+
+function checkR(){
+    let number = 404;
+    rButtons.forEach(function (input){
+        if(input.matches(":checked")){
+            number = Number.parseFloat(input.value);
+        }
+    })
+
+    if(number === 404){
+        return {
+            status: false,
+            value: number,
+            errorMessage: "Выберете R!"
+        }
+    }
+    else return {
+        status: true,
+        value: number,
+        errorMessage: ""
+    }
+}
+
+function send(array){
+    request.open("GET", "https://kaplaan.ru/script.php")
+    request.send(array)
+
 }
