81a0ecb501a6cd36393b150cb2b837e2af56ef4b
dmitrykap1an <dk475641@gmail.com>
2022-09-16 23:49:04 +0300

    fixed index.js and index.html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 
-let xButtons = document.querySelectorAll(".x_button");
-let rButtons = document.querySelectorAll(".r-button");
-let yButton = document.querySelector(".y_text");
-let form = document.getElementById("form")
-let table = document.querySelector(".my_table")
-const tbody = table.getElementsByTagName("tbody")[0];
+const xButtons = document.querySelectorAll(".x_button"),
+    rButtons = document.querySelectorAll(".r-button"),
+    yButton = document.querySelector(".y_text"),
+    form = document.getElementById("form"),
+    table = document.querySelector(".my_table"),
+    tbody = table.getElementsByTagName("tbody")[0];
 
-form.onsubmit= function (){
+form.addEventListener("click",onsubmit);
+
+function onsubmit(){
 
     let x = checkX()
     let y = validateY()
@@ -25,6 +27,7 @@ form.onsubmit= function (){
 
         alert(errorString)
     }
+    return false;
 }
 
 function validateY(){
@@ -42,7 +45,7 @@ function validateY(){
 }
 
 function checkY(yVal){
-    if(yVal >= -5 && yVal <= 3 && yVal !== ""){
+    if(yVal >= -5 && yVal <= 3){
         return {
             status: true,
             value: yVal,
@@ -106,45 +109,38 @@ function checkR(){
 }
 function updateTable(response) {
     const row = document.createElement("tr");
-    const cell_x = document.createElement("td");
-    const cell_y = document.createElement("td");
-    const cell_R = document.createElement("td");
-    const cell_hit = document.createElement("td");
-    const cell_request_time = document.createElement("td");
-    const cell_timing = document.createElement("td");
-    cell_x.innerHTML = response.x;
-    cell_y.innerHTML = response.y;
-    cell_R.innerHTML = response.R;
-    cell_hit.innerHTML = response.result;
-    cell_request_time.innerHTML = response.server_time;
-    cell_timing.innerHTML = response.script_exec_time + "ms";
-    row.appendChild(cell_x);
-    row.appendChild(cell_y);
-    row.appendChild(cell_R);
-    row.appendChild(cell_hit);
-    row.appendChild(cell_request_time);
-    row.appendChild(cell_timing);
+    const x = document.createElement("td");
+    const y = document.createElement("td");
+    const R = document.createElement("td");
+    const hit = document.createElement("td");
+    const request_time = document.createElement("td");
+    const timing = document.createElement("td");
+    x.innerHTML = response.x;
+    y.innerHTML = response.y;
+    R.innerHTML = response.R;
+    hit.innerHTML = response.result;
+    request_time.innerHTML = response.server_time;
+    timing.innerHTML = response.script_exec_time + "ms";
+    row.appendChild(x);
+    row.appendChild(y);
+    row.appendChild(R);
+    row.appendChild(hit);
+    row.appendChild(request_time);
+    row.appendChild(timing);
     tbody.insertBefore(row, tbody.firstChild);
 }
 
 function getResponse(request){
     console.log(request.responseText);
     const response = JSON.parse(request.responseText)
-    if (response.correct === "true") {
-        updateTable(response);
-    } else {
-        alert("Ошибка обработки запроса сервером");
-    }
-
+    updateTable(response)
 }
 function sendRequest(array){
 
-    const path = "script.php?x=" + array[0].value + "&y=" + array[1].value + "&R=" + array[2].value
+    const path = "script.php?x=" + array[0] + "&y=" + array[1] + "&R=" + array[2]
     const request = new XMLHttpRequest()
     request.open("GET", path, true)
 
-    request.responseType = "text"
-    request.setRequestHeader("Content-Type", "application/x-www-form-url")
     request.onreadystatechange = () =>{
         if(request.readyState === 4 && request.status === 200){
             getResponse(request)
