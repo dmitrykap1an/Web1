ff9ad030cb92d1bb6b8ced49d78773278973f8f7
dmitrykap1an <dk475641@gmail.com>
2022-09-21 22:38:38 +0300

    php: major rework

--- src/script.php
+++ src/script.php
@@ -1,46 +1,103 @@
 <?php
 date_default_timezone_set('UTC');
-$time_enter = microtime(true);
+define("time_enter", hrtime(true));
 
+$db = new SQLite3("/tmp/web1.db");
+checkBD($db);
+
+if (isset($_COOKIE["session"])){
+    define("cookie",  $_COOKIE["session"]);
+}
+else {
+
+    define("cookie", bin2hex(random_bytes(512)));
+    setcookie("ID", cookie, array('samesite' => 'Strict'));
+}
+if(!((isset($_GET["x"]) && isset($_GET["y"]) && isset($_GET["R"])) || isset($_GET["restore"]))){
+    exit("Ошибка получения данных");
+}
 $x = (float) $_GET['x'];
 $y = (float) $_GET['y'];
 $R = (float) $_GET['R'];
+$restore = isset($_GET["restore"]);
 
-function encode_json($array)
-{
-  return
-    "{" .
-    "\"x\":\"" . (string)$array[0] . "\"," .
-    "\"y\":\"" . (string)$array[1] . "\"," .
-    "\"R\":\"" . (string)$array[2] . "\"," .
-    "\"result\":\"" . $array[3] . "\"," .
-    "\"server_time\":\"" . (string)$array[4] . "\"," .
-    "\"script_exec_time\":\"" . (string)$array[5] . "\"," .
-    "\"correct\":\"" . $array[6] . "\"" .
-    "}";
+if(!$restore){
+
+    if (!(($x <= 2 && $x >= -2) &&
+        ($y <= 3 && $y >= -5) &&
+        ($R == 1 || $R == 1.5 || $R == 2 || $R == 2.5 || $R == 3)))
+    {
+        echo json_encode(
+            [
+                "x" => $x,
+                "y" => $y,
+                "R" => $R,
+                "result" => 0,
+                "server_time" => 0,
+                "execute_time" => 0,
+                "success" => false,
+            ]);
+    }
+    else {
+        if (
+            ($x >= 0 && $y >= 0 && $x + $y < $R && $x <= $R) ||
+            ($x >= 0 && $y <= 0 && ($x * $x + $y * $y) <= ($R/2 * $R/2)) ||
+            ($x <= $R/2 && $y <= $R))
+        {
+            $result = "Точка попала в область";
+        } else {
+            $result = "Точка не попала в область";
+        }
+
+        $server_time = date('e H:i:s', time());
+        $execute_time = (string) ceil(((hrtime(true) - time_enter)) /10 ** 6);
+
+        $statementSt = $db->prepare(
+            "INSERT INTO"
+            . " web1table(cookieID, resultJson)"
+            . " VALUES"
+            . " (:cookieID, :resultJson)"
+        );
+        $statementSt->bindValue("cookieID", cookie, SQLITE3_TEXT);
+        $statementSt->bindValue("resultJson", json_encode(
+            [
+                "x" => $x,
+                "y" => $y,
+                "R" => $R,
+                "result" => $result,
+                "server_time" => $server_time,
+                "execute_time" => $execute_time,
+                "success" => true,
+            ]), SQLITE3_TEXT);
+        $statementSt->execute()->finalize();
+        echo json_encode(
+            [
+                "x" => $x,
+                "y" => $y,
+                "R" => $R,
+                "result" => $result,
+                "server_time" => $server_time,
+                "execute_time" => $execute_time,
+                "success" => true,
+            ]);
+    }
+
+}
+else{
+    $getStatement = $db->prepare(
+        "SELECT resultJson FROM web1table WHERE cookieID=:id"
+    );
+    $getStatement->bindValue(":id", cookie, SQLITE3_TEXT);
+    echo $getStatement->execute();
 }
 
-if (!(($x <= 2 && $x >= -2) &&
-     ($y <= 3 && $y >= -5) &&
-    ($R == 1 || $R == 1.5 || $R == 2 || $R == 2.5 || $R == 3)))
-{
-  echo encode_json(array(0, 0, 0, 0, 0, 0, "false"));
-}
-else {
-  if (
-    ($x >= 0 && $y >= 0 && $x + $y < $R && $x <= $R && $x <= $R ) ||
-    ($x >= 0 && $y <= 0 && ($x * $x + $y * $y) <= ($R/2 * $R/2)) ||
-    ($x <= $R/2 && $y <= $R))
-  {
-    $result = "Match";
-  } else {
-    $result = "No match";
-  }
-
-  $server_time = date('e H:i:s', time());
-  $script_exec_time = ceil((10 ** 6 * (microtime(true) - $time_enter)));
-
-  echo encode_json(array(
-    $x, $y, $R, $result, $server_time, $script_exec_time, "true"
-  ));
+function checkBD($db){
+    $db->exec(
+        "CREATE TABLE IF NOT EXISTS"
+        . " web1table"
+        . "("
+        . "cookieID STRING"
+        . ", resultJson STRING"
+        . ")"
+    );
 }
